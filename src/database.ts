c659129936314186338a1c5df480b122f29838f7
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-10-01 09:55:46 -0700

    initial setup of database connection

---
+++ src/database.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
+
+dotenv.config()
+
+const {
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_DB_test,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    ENV
+} = process.env
+
+let database
+console.log(`Environment: ${ENV}`)
+
+if (ENV === 'dev') {
+    database = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    })
+}
+
+if (ENV === 'test') {
+    database = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB_test,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    })
+}
+
+export default database
