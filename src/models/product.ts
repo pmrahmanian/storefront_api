f391200bf0b9ffc06650b563e7a7736f7db0e0c5
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-10-03 12:44:46 -0700

    added create model

--- src/models/product.ts
+++ src/models/product.ts
@@ -25,7 +25,7 @@ export class ProductStore {
     async show(id:string): Promise<Product> {
         try {
             const conn = await database.connect()
-            const sql = 'SELECT * FROM products WHERE id=($1)'
+            const sql = 'SELECT * FROM products WHERE id=($1);'
             const result = await conn.query(sql, [id])
             conn.release()
             return result.rows[0]
@@ -34,4 +34,15 @@ export class ProductStore {
         }
     }
 
+    async create (p:Product): Promise<Product> {
+        try {
+            const conn = await database.connect()
+            const sql = `INSERT INTO products (name, price, category) VALUES ($1, $2, $3) RETURNING *;`
+            const result = await conn.query(sql, [p.name, p.price, p.category])
+            conn.release()
+            return result.rows[0]
+        } catch (error) {
+            throw new Error(`Could not add new product ${p.name}. Error: ${error}`)
+        }
+    }
 }
