9efe65f114706ff1f0517553fbf15d5d70492b89
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-10-03 12:34:10 -0700

    added show model

--- src/models/product.ts
+++ src/models/product.ts
@@ -8,6 +8,7 @@ export type Product = {
 }
 
 export class ProductStore {
+
     async index(): Promise<Product[]> {
         try { 
             const conn = await database.connect()
@@ -19,4 +20,18 @@ export class ProductStore {
             throw new Error(`Cannot connect to database: ${error}`)
         }
     }
+
+
+    async show(id:string): Promise<Product> {
+        try {
+            const conn = await database.connect()
+            const sql = 'SELECT * FROM products WHERE id=($1)'
+            const result = await conn.query(sql, [id])
+            conn.release()
+            return result.rows[0]
+        } catch (error) {
+            throw new Error(`Could not find book ${id}. Error: ${error}`)
+        }
+    }
+
 }
