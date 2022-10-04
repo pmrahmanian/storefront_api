ac119479dd582ef1fd2eac48ba53b297932cfb1f
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-10-04 14:45:27 -0700

    defined order handler

---
+++ src/handlers/order.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response } from 'express'
+import { Order, OrderStore } from '../models/order'
+
+const store = new OrderStore()
+
+// handlers
+const index = async (req:Request, res:Response) => {
+    const user_id = req.params.user
+    const status = req.params.status
+
+    // @ts-ignore
+    let orders;
+
+    if (user_id) {
+        if (status === 'complete') {
+            orders = await store.completedOrdersForUser(user_id)
+        } else {
+            orders = await store.currentOrderForUser(user_id)
+        }
+    } else {
+        orders = await store.index()
+    }
+    // @ts-ignore
+    res.status(200).json(orders)
+}
+
+const show = async (req:Request, res:Response) => {
+    // @ts-ignore
+    const order = await store.show(req.params.id)
+    // @ts-ignore
+    res.status(200).json(order)
+}
+
+const create = async (req:Request, res:Response) => {
+    try {
+        const order:Order = {
+            status: req.body.status, 
+            user_id: req.body.user_id
+        }
+
+        const newOrder = await store.create(order)
+        res.json(newOrder)
+    } catch (error) {
+        res.status(400)
+        res.json(error)
+    }
+}
+
+const destroy = async (req:Request, res:Response) => {
+    const deleted = await store.delete(req.body.id)
+    res.json(deleted)
+}
+
+// routes
+const user_routes = (app: Express.Application) => {
+    app.get('/users', index)
+    app.get('/users/:id', show)
+    app.post('/users', create)
+    app.delete('/users/:id', destroy)
+}
+
+export default user_routes
