46e559bbfd3e8ed1e106f5de0ed618f9ce8769c4
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-10-04 18:36:31 -0700

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 import user_routes from './handlers/user'
 import order_routes from './handlers/order'
@@ -7,6 +8,14 @@ import product_routes from './handlers/product'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
+
+const corsOptions = {
+	Origin: true, //allow all cross origins
+	optionsSuccessStatus: 200
+}
+// apply to all routes
+app.use(cors(corsOptions)) 
+
 
 app.use(bodyParser.json())
 
